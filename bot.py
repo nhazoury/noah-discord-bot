80a5cabea2c098ed6acb8e17ea6e44bb43df496a
nhazoury <n.hazoury@live.com>
2021-11-15 17:13:49 +0000

    Fixed dad joke bug where only im is used

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ async def on_message(message):
         if bot.im_dad and message.content != (command_prefix + 'imdad'):
             cut_message = message.content
             for i in range(0, len(imList)):
-                cut_message = cut_message.split('im', 1)[-1]
+                cut_message = cut_message.split(imList[i], 1)[-1]
             response = 'Hi,' + cut_message + ', I\'m dad!'
             await message.channel.send(response)
     
